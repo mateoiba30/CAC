c11a6d7d6eb969e6511f018f468b84173ebf7e54
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-12 14:48:13 -0300

    corrijo ej 9

--- mod2_winmips_multiciclo/ej9p4-while.s
+++ mod2_winmips_multiciclo/ej9p4-while.s
@@ -9,8 +9,7 @@ ld r1,a(r0) #alejo r1 y r4 para no tener RAW
 ld r2,x(r0)
 ld r3,y(r0)
 
-while: slt r4,r1,r0 # pone en 1 r4 si 'a' es menor a cero
-beqz r1,fin # termina si 'a' es igual a cero
+while: beqz r1,fin # termina si 'a' es igual a cero
 daddi r1,r1,-1 # delay slot, va a restar una vez demás a 'a', pero va a tenrmiar cuando quiero y sin stalls
 j while
 dadd r2,r2,r3# delay slot
