98a5f7388ae2992703aed66507012141651ad93d
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-13 13:51:16 -0300

    anda perfecto sin atascos solo con delay slot

--- mod2_winmips_multiciclo/ej10p4.s
+++ mod2_winmips_multiciclo/ej10p4.s
@@ -7,10 +7,10 @@ cant: .word 0
 
 .code
 lbu r1,cadena(r0)# carga inicial para la 1er comparacion del loop 
+daddi r4,r0,17 # diml + 1 porque inicio restando# la pongo 2da para que no haya RAW con el loop
 dadd r5,r0,r0 # paso # la pongo 1ra para que no haya RAW con el loop
 lbu r2,car(r0) #caracter a comparar
 dadd r3,r0,r0 #contador aciertos
-daddi r4,r0,16 # diml
 
 loop: daddi r4,r4,-1
 bne r1,r2, salto# si no son iguales # lo alejo de la instr 14 para quew no haya RAW
@@ -21,3 +21,5 @@ daddi r5,r5,1 # paso de 1 porque son bytes #delay slot
 
 sd r3, cant(r0)# sb es para byte, para un caracter, esto es una word #unica instruccion con RAW si no hy forwarding
 halt
+
+#con delay slot y sin forwarding no tengo atascos
