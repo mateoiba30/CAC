24fd009d7966005999d8787090cfd1db1e15b7de
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-13 13:48:10 -0300

    solo 2 RAWs y nada mas sin forwarding y con delay slot

--- mod2_winmips_multiciclo/ej10p4.s
+++ mod2_winmips_multiciclo/ej10p4.s
@@ -6,14 +6,15 @@ cant: .word 0
 #lbu para cargar caracteres en registros
 
 .code
+lbu r1,cadena(r0)# carga inicial para la 1er comparacion del loop 
 dadd r5,r0,r0 # paso # la pongo 1ra para que no haya RAW con el loop
 lbu r2,car(r0) #caracter a comparar
 dadd r3,r0,r0 #contador aciertos
 daddi r4,r0,16 # diml
 
-loop: lbu r1,cadena(r5) #caracter actual
+loop: daddi r4,r4,-1
 bne r1,r2, salto# si no son iguales # lo alejo de la instr 14 para quew no haya RAW
-daddi r4,r4,-1 #delay slot
+lbu r1,cadena(r5) #caracter actual #delay slot
 daddi r3,r3,1
 salto: bnez r4, loop #si no recorrí todo, repito
 daddi r5,r5,1 # paso de 1 porque son bytes #delay slot
