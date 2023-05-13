0725ff7076eecb69e153ebc9d0c09982de1c082f
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-13 13:45:18 -0300

    vuelvo a tener RAW, pero sin brucnh taken

--- mod2_winmips_multiciclo/ej10p4.s
+++ mod2_winmips_multiciclo/ej10p4.s
@@ -12,11 +12,11 @@ dadd r3,r0,r0 #contador aciertos
 daddi r4,r0,16 # diml
 
 loop: lbu r1,cadena(r5) #caracter actual
-daddi r5,r5,1 # paso de 1 porque son bytes
-daddi r4,r4,-1
 bne r1,r2, salto# si no son iguales # lo alejo de la instr 14 para quew no haya RAW
+daddi r4,r4,-1 #delay slot
 daddi r3,r3,1
 salto: bnez r4, loop #si no recorrí todo, repito
+daddi r5,r5,1 # paso de 1 porque son bytes #delay slot
 
 sd r3, cant(r0)# sb es para byte, para un caracter, esto es una word #unica instruccion con RAW si no hy forwarding
 halt
