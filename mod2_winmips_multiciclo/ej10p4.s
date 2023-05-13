c1b884ab9bc0a688d20496047b97289687a1f401
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-13 13:41:42 -0300

    solo 1 RAW sin forwarding

--- mod2_winmips_multiciclo/ej10p4.s
+++ mod2_winmips_multiciclo/ej10p4.s
@@ -6,17 +6,17 @@ cant: .word 0
 #lbu para cargar caracteres en registros
 
 .code
+dadd r5,r0,r0 # paso # la pongo 1ra para que no haya RAW con el loop
 lbu r2,car(r0) #caracter a comparar
 dadd r3,r0,r0 #contador aciertos
 daddi r4,r0,16 # diml
-dadd r5,r0,r0 # paso
 
 loop: lbu r1,cadena(r5) #caracter actual
-bne r1,r2, salto# si no son iguales
-daddi r3,r3,1
-salto: daddi r5,r5,1 # paso de 1 porque son bytes
+daddi r5,r5,1 # paso de 1 porque son bytes
 daddi r4,r4,-1
-bnez r4, loop #si no recorrí todo, repito
+bne r1,r2, salto# si no son iguales # lo alejo de la instr 14 para quew no haya RAW
+daddi r3,r3,1
+salto: bnez r4, loop #si no recorrí todo, repito
 
-sd r3, cant(r0)# sb es para byte, para un caracter, esto es una word
+sd r3, cant(r0)# sb es para byte, para un caracter, esto es una word #unica instruccion con RAW si no hy forwarding
 halt
