679fe22707c106fd1ae10458a03a974ab5fa7b9c
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-20 18:29:10 -0300

    termino de comentar ej1

--- mod2_winmips_multiciclo/practica5/ej1p5v2.s
+++ mod2_winmips_multiciclo/practica5/ej1p5v2.s
@@ -7,9 +7,9 @@ res2: .double 0.0
 .code
 l.d f1, n1(r0)
 l.d f2, n2(r0)
-#nop me soluciona RAW y WAR de las 2 lineas de abajo
+#nop me soluciona RAW y WAR de las 2 lineas de abajo. soluciona el WAR porq el add no se atrasa ahciendo que no coincida con la de mul (el RAW de add nunca atrasa a mul)
 add.d f3, f2, f1 #sumo, con RAW porque espera a f2 y tarda varias etapas por operar con flotantes
-mul.d f1, f2, f1 #WAR debido a que quiere escribir f1, pero no está disponible porque la isntr de arriba se atraso con la lectura de registros 
+mul.d f1, f2, f1 #WAR debido a que quiere escribir f1, pero no está disponible porque la isntr de arriba se atraso con la lectura de registros. en realidad para cuando llega el WB de esta instr ya se terminó la elctura de la instr anterior, pero el ssimluador por las dudas atasca 
 #esta instruccion hace que la siguiente mul tenga muchos RAW porque espera a poder usar los registros de esta linea (por eso es RAW y no str)
 
 mul.d f4, f2, f1 # multiplico, sin stall pero tarda muchas etapas
