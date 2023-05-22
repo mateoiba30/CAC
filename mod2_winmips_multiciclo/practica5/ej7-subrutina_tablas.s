66fc54e0cd2fc195547e399adff62b0636759754
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-22 16:01:33 -0300

    anda sin brunch takne

--- mod2_winmips_multiciclo/practica5/ej7-subrutina_tablas.s
+++ mod2_winmips_multiciclo/practica5/ej7-subrutina_tablas.s
@@ -18,9 +18,9 @@ beqz $t1, salto #si es cero es porq no es menor que M
 daddi $a1, $a1, 8 #paso, lo puedo alterar porque es $a #delay slot
 daddi $v0, $v0, 1 # si la resta da cero aumento el contador
 
-salto: daddi $a3, $a3, -1 #diml # delay slot
-bnez $a3, CONTAR
+salto: bnez $a3, CONTAR
+daddi $a3, $a3, -1 #diml # delay slot
 jr $ra
 
 #no olvidar que el paso es de a 8 para word!
-#con delay slot
+#con delay slot y forwarding
