6d12ddc893d14447873a8a1b2471ca1262feac50
Mateo Ibanez <mateoiba30@gmail.com>
2023-05-22 19:26:56 -0300

    anda ej8 sin atascos

--- mod2_winmips_multiciclo/practica5/ej8-subrutinas_cadenas.s
+++ mod2_winmips_multiciclo/practica5/ej8-subrutinas_cadenas.s
@@ -1,6 +1,6 @@
 .data
 CADENA0: .asciiz "aeiou"
-CADENA1: .asciiz "aeiou0"
+CADENA1: .asciiz "aEio0"
 POS: .word 0
 
 .code
@@ -13,19 +13,24 @@ halt
 ANALIZAR_STRINGS: lbu $t0,0($a0)
 lbu $t1,0($a1)
 
-bne $t0, $t1, fin #si son distintos termino
+nop #para evitar el RAW en bezq
 beqz $t0, fin2 #me fijo si termine una de las cadenas
-beqz $t1, fin2
+daddi $a0, $a0, 1 #avanzo de paso #delay slot
 
-daddi $v0, $v0, 1 #incremento pos de diferencia
-daddi $a0, $a0, 1 #avanzo de paso
-daddi $a1, $a1, 1
+beqz $t1, fin2
+daddi $a1, $a1, 1 #delay slot
+
+bne $t0, $t1, fin #si son distintos termino #esta comparacion la hago ultima para alejarla del lbu y no tener RAW
+daddi $v0, $v0, 1 #incremento pos de diferencia #delay slot
+
 j ANALIZAR_STRINGS
+nop #delay slot
 
 fin2: daddi $v0, $0, -1
-fin: jr $ra
+fin: daddi $v0, $v0, -1#resto 1 porque siempre cuma al menos una vez en el delay slot
+jr $ra
 
 #usar lbu para caracteres, y paso 1
-#sin delay slot
+#con delay slot
 #si una es mas larga que otra van a diferir al comparar el 0 con un caracteres
 #el caracter que vale cero es el de fin de string
